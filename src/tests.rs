cdd6b60a4ae1f5f2826d12023020aab8105cd980
Arkid Kaleci <kaleciarkid@gmail.com>
2022-08-16 22:46:07 +0200

    chore: Add tests for main library

---
+++ src/tests.rs
@@ -0,0 +1,45 @@
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_eta_new() {
+        let eta = Eta::new(10, TimeAcc::SEC);
+        assert_eq!(eta.tasks_count, 10);
+        assert_eq!(eta.tasks_done, 0);
+        assert_eq!(eta.time_accuracy, TimeAcc::SEC);
+        assert_eq!(eta.total_time_elapsed, 0);
+        assert_eq!(eta.paused, None);
+    }
+
+    #[test]
+    fn test_eta_in_progress() {
+        let eta = Eta::in_progress(10, TimeAcc::SEC, 5);
+        assert_eq!(eta.tasks_count, 10);
+        assert_eq!(eta.tasks_done, 5);
+        assert_eq!(eta.time_accuracy, TimeAcc::SEC);
+        assert_eq!(eta.total_time_elapsed, 0);
+        assert_eq!(eta.paused, None);
+    }
+
+    #[test]
+    fn test_eta_step() {
+        let mut eta = Eta::new(10, TimeAcc::SEC);
+        eta.step();
+        assert_eq!(eta.tasks_done, 1);
+    }
+
+    #[test]
+    fn test_eta_elapsed() {
+        let mut eta = Eta::new(10, TimeAcc::SEC);
+        eta.step();
+        assert!(eta.elapsed() > 0);
+    }
+
+    #[test]
+    fn test_eta_elapsed_time() {
+        let mut eta = Eta::new(10, TimeAcc::SEC);
+        eta.step();
+        assert!(eta.total_time_elapsed > 0);
+    }
+}
