3a18eb90872bc2904e63bce167ed6a3baf418ddf
Arkid Kaleci <kaleciarkid@gmail.com>
2022-08-19 21:01:42 +0200

    docs: Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,49 @@
 #![allow(dead_code)]
 #![doc(html_favicon_url = "images/favicon.png")]
 #![doc(html_logo_url = "images/logo.png")]
+#![warn(missing_docs)]
+
+//!Tracking progress on repetive tasks and measuring remaining times.
+//!
+//!# Usage
+//! Add this to your `Cargo.toml`:
+//! ```toml
+//! [dependencies]
+//! eta = "0.2.1"
+//! ```
+//! and this to your source code:
+//! ```rust
+//! use eta::Eta;
+//! ```
+//! 
+//! # Example
+//! ```rust
+//! use eta::Eta;
+//! 
+//! fn calculate_square {
+//!    //--snip--
+//! }
+//! 
+//! fn main() {
+//! let count = 100;
+//! let numbers = Vec::from_iter(0..count);
+//! let mut eta = Eta::new(count, TimeAcc::MILLI);
+//! 
+//! for number in numbers {
+//!     calculate_square(number);
+//!     eta.step();
+//!     if (number % 10) == 0 {
+//!     println!("{}", eta);
+//! }
 
 mod tests;
 
 use std::time::{Instant};
 
+
 #[derive(Debug, Clone, PartialEq, Eq)]
+/// `Eta` is the main object which keep track of task count and elapsed times.
+/// It implements several methods to display various information about the progress.
 pub struct Eta {
     tasks_count: usize,
     tasks_done: usize,
@@ -17,15 +54,17 @@ pub struct Eta {
     paused: bool
 }
 
-/* pub struct EtaMessageFormat {
-    header: String,
-    footing: String,
-    accuracy: TimeAcc,
-} */
-
 #[derive(Clone, Debug, PartialEq, Eq)]
+///`TimeAcc` is an enum for keeping track of the grade of accuracy for any information regarding time.
 pub enum TimeAcc {
-    SEC, MILLI, MICRO, NANO
+    ///SEC stands for seconds. 1 seconds has 1000 milliseconds.
+    SEC,
+    ///MILLI stands for milliseconds.
+    MILLI,
+    ///MICRO stands for microseconds. 1,000 microseconds make up 1 millisecond
+    MICRO,
+    ///NANO stands for nanoseconds. 1,000,000 nanoseconds make up 1 millisecond.
+    NANO
 }
 
 impl Eta {
@@ -41,10 +80,29 @@ impl Eta {
         }
     }
 
+    ///Creates a new `Eta` object with the given number of tasks and accuracy on time measurement.
+    /// 
+    /// # Example
+    /// ```rust
+    /// let eta = Eta::new(count_of_tasks, TimeAcc::MILLI);
+    /// ```
+
     pub fn new (tasks_count: usize, time_accuracy: TimeAcc) -> Eta {
         Eta::create_instance(tasks_count, time_accuracy, 0)
     }
 
+    /// Pauses time measurement for the object. Resuming time measurement is done by calling `resume()`.
+    /// Time between pauses is kept track of and not reset. You can step `Eta` while it's paused.
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut eta = Eta::new(count_of_tasks, TimeAcc::MILLI);
+    /// sleep(Duration::from_secs(1)); // one second elapses
+    /// eta.pause();
+    /// sleep(Duration::from_secs(1)); // this other second isn't kept track of
+    /// eta.step(); // time elapsed will account for approx. 1 second here
+    /// eta.resume(); // does nothing as eta is resumed automatically right after eta.step()
+    /// ```
     pub fn pause(&mut self) {
         if !self.paused {
             self.paused = true;
@@ -52,6 +110,18 @@ impl Eta {
         }
     }
 
+    /// Resumes time measurement for the object. Must be called after pausing with eta.pause(),
+    /// otherwise it will do nothing. Object is resumed automatically after eta.step().
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut eta = Eta::new(count_of_tasks, TimeAcc::MILLI);
+    /// eta.pause(); // eta is paused
+    /// sleep(Duration::from_secs(1)); // this second is NOT kept track of
+    /// eta.resume(); // resumes the object
+    /// sleep(Duration::from_secs(1)); // this other second is kept track of
+    /// eta.step(); // time elapsed will account for approx. 1 seconds here
+    /// ```
     pub fn resume(&mut self) {
         if self.paused {
             self.recent_time = Instant::now();
@@ -59,6 +129,16 @@ impl Eta {
         }
     }
 
+    /// Steps the eta object by one task and does all the time calculations at the time of being called.
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut eta = Eta::new(count_of_tasks, TimeAcc::MILLI);
+    /// for something in 0..count_of_tasks {
+    ///     // do something
+    ///     eta.step();
+    ///     println!("{}", eta);
+    /// }
     pub fn step(&mut self) {
         self.tasks_done += 1;
         if !self.paused {
@@ -79,10 +159,31 @@ impl Eta {
         }
     }
 
+    /// Returns the portion of tasks that have been completed as a double between 0 and 1.
+    /// Multiply by 100 to get the according percentage.
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut eta = Eta.new(count_of_tasks, TimeAcc::MILLI)
+    /// for something in 0..count_of_tasks {
+    ///     // do something
+    ///     eta.step();
+    ///     println!("{}% of the job is done", eta.progress()*100);
+    /// }
     pub fn progress(&self) -> f64 {
         (self.tasks_done as f64) / (self.tasks_count as f64)
     }
 
+    /// Returns the estimated amount of time left for the job.
+    /// 
+    /// # Example
+    /// ```rust
+    /// let mut eta = Eta.new(count_of_tasks, TimeAcc::MILLI)
+    /// for something in 0..count_of_tasks {
+    ///     // do something
+    ///     eta.step();
+    ///     println!("Job will be finished in {}s.", eta.time_remaining()/1000);
+    /// }
     pub fn time_remaining(&self) -> usize {
         ((self.tasks_count - self.tasks_done) as f64 * (self.total_time_elapsed as f64) / (self.tasks_done as f64))
             as usize
